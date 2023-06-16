127f1ceb3031a6a47deee91c50a6d3699a040f81
Piyush Gupta <122008271+PIYUSH-0612@users.noreply.github.com>
2023-06-17 00:00:19 +0530

    Add files via upload

---
+++ javacalcii.java
@@ -0,0 +1,17 @@
+class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		int a=10;
+        int b=12;
+        int c=a+b;
+        int d=a-b;
+        int e=a/b;
+        int f=a*b;
+        System.out.println("the addition of "+a+" and "+b+" is "+c);
+        System.out.println("the substraction of "+a+" and "+b+" is "+d);
+        System.out.println("the division of "+a+" and "+b+" is "+e);
+        System.out.println("the product of "+a+" and "+b+" is "+f);
+	}
+}
